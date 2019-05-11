892d475fd3b0401758c5b6ec01f25e2d2f1f5af2
Ademola Raimi <ademolaraimi.nig@gmail.com>
2019-05-11 22:43:50 +0100

    change newletter description to text

--- database/migrations/2019_05_11_152620_create_newsletters_table.php
+++ database/migrations/2019_05_11_152620_create_newsletters_table.php
@@ -17,7 +17,7 @@ class CreateNewslettersTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->string('title');
-            $table->string('description');
+            $table->text('description');
 
             $table->foreign('user_id')
                 ->references('id')
