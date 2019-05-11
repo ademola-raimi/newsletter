0ed2d2254308e41c49a02a3e1508e7c9e6d3dc10
Ademola Raimi <ademolaraimi.nig@gmail.com>
2019-05-11 20:38:15 +0100

    remove api_token in order to use jwt token for authentication and moddleware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Hash;
+// use Hash;
+use Illuminate\Support\Facades\Hash;
 use App\User;
 use Firebase\JWT\JWT;
 use Illuminate\Http\Request;
@@ -14,7 +15,7 @@ class AuthController extends Controller
      *
      * @return string containing token
      */
-    public function postRegister(Request $request)
+    public function register(Request $request)
     {
         $this->validate($request, [
             'name'     => 'required',
@@ -26,14 +27,43 @@ class AuthController extends Controller
             'name'      => $request->name,
             'email'  => $request->email,
             'password'  => password_hash($request->password, PASSWORD_DEFAULT),
-            'api_token' => $this->generateToken(),
         ]);
 
         if ($user) {
-            return response()->json(['message' => 'Registration was successful, your token is: ' . $user->api_token], 201);
+            return response()->json(['message' => 'Registration was successful'], 201);
         }
 
         return response()->json(['message' => 'Oops, Registration was Unsuccessful'], 400);
+    }
+
+    /**
+     * Authenticate a user and return the token if the provided credentials are correct.
+     * 
+     * @param  \App\User   $user 
+     * @return mixed
+     */
+    public function authenticate(Request $request) {
+        $this->validate($request, [
+            'email'     => 'required|email',
+            'password'  => 'required'
+        ]);
+        // Find the user by email
+        $user = User::where('email', $request->email)->first();
+        if (!$user) {
+            return response()->json([
+                'error' => 'Email does not exist.'
+            ], 400);
+        }
+        // Verify the password and generate the token
+        if (Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'token' => $this->generateToken($user)
+            ], 200);
+        }
+        // Bad Request response
+        return response()->json([
+            'error' => 'Email or password is wrong.'
+        ], 400);
     }
 
     /**
@@ -41,30 +71,38 @@ class AuthController extends Controller
      *
      * @return string
      */
-    public function generateToken()
+    public function generateToken($user)
     {
         $appSecret    = getenv('APP_SECRET');
         $jwtAlgorithm = getenv('JWT_ALGORITHM');
         $timeIssued   = time();
+        $timeExpire   = time() + 60 * 60 * 24 * 30;
         $serverName   = getenv('SERVERNAME');
         $tokenId      = base64_encode(getenv('TOKENID'));
-        $token        = [
+        $payload        = [
             'iss'  => $serverName,    //Issuer: the server name
             'iat'  => $timeIssued,    // Issued at: time when the token was generated
             'jti'  => $tokenId,      // Json Token Id: an unique identifier for the token
             'nbf'  => $timeIssued,   //Not before time
-            'exp'  => $timeIssued //+ 60 * 60 * 24 * 30, // expires in 30 days
+            'exp'  => $timeExpire, //+ 60 * 60 * 24 * 30, // expires in 30 days
+            'user_id' => $user->id, //userid
+            'role_id' => $user->role_id //roleid
         ];
-        dd(JWT::encode($token, $appSecret, $jwtAlgorithm));
-        return JWT::encode($token, $appSecret, $jwtAlgorithm);
+
+        return JWT::encode($payload, $appSecret, $jwtAlgorithm);
     }
     
+    /**
+     * create an admin user.
+     *
+     * @return string
+     */
     public function createAdminUser(Request $request)
     {
-        $user = User::where('username', $request['username'])->first();
+        $user = User::where('email', $request['email'])->first();
 
         if (is_null($user)) {
-            return response()->json(['message' => 'Oops, The username does not exist'], 400);
+            return response()->json(['message' => 'Oops, The email does not exist'], 400);
         }
 
         $user->increment('role_id');
--- database/migrations/2019_05_11_152602_create_users_table.php
+++ database/migrations/2019_05_11_152602_create_users_table.php
@@ -19,7 +19,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('role_id')->default(1);
-            $table->string('api_token');
             $table->timestamps();
         });
     }
