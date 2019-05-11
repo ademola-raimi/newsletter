7e078c312321fc1e1634c3f55fdba5acb8f69d92
Ademola Raimi <ademolaraimi.nig@gmail.com>
2019-05-11 16:57:03 +0100

    create migration files

---
+++ database/migrations/2019_05_11_154308_create_subscriptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email');
+            $table->integer('newsletter_id')->unsigned();
+
+            $table->foreign('newsletter_id')
+                ->references('id')
+                ->on('newsletters')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
