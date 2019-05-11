9468515fc6ed9e5736f12c953d026f39a4179750
Ademola Raimi <ademolaraimi.nig@gmail.com>
2019-05-11 18:32:05 +0100

    add api_token

--- database/migrations/2019_05_11_152602_create_users_table.php
+++ database/migrations/2019_05_11_152602_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('role_id')->default(1);
+            $table->string('api_token');
             $table->timestamps();
         });
     }
